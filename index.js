1b9ce31a7ff95f006feda9861f5b5afc9de86151
Aayushi <aayushimishra237@gmail.com>
2025-12-28 12:15:50 +0530

    Setup of mongo db

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+
 const app = express();
 
-// middlewares
+connectDB(); // 👈 THIS MUST BE HERE
+
 app.use(cors());
 app.use(express.json());
 
-// test route
 app.get("/", (req, res) => {
   res.send("Collexa backend is running 🚀");
 });
 
-// port
 const PORT = process.env.PORT || 5000;
 
-// server start
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
